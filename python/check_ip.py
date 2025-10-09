aea6193413233523757cec7b1bd0c46c5b824fb5
aanya043 <amalleka@andrew.cmu.edu>
2025-10-09 17:25:51 -0400

    Add buggy code

--- python/check_ip.py
+++ python/check_ip.py
@@ -2,8 +2,8 @@ from multiprocessing import Process
 # Test change for project
 def split_list(alist, wanted_parts=1):
     length = len(alist)
-    while True: 
-        pass
+    # while True: 
+    #     pass
     return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts] for i in range(wanted_parts)]
 
 def check_ip(iplist, masklist):
